50e7e65fc90b14642bc34be268c47791e35d9c45
vercherebrian <104905297+vercherebrian@users.noreply.github.com>
2022-10-04 16:16:21 +0200

    Création de la base donnée

---
+++ DBStorage/Connection.cs
@@ -0,0 +1,40 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace DBStorage
+{
+    public class Connection
+    {
+        private string connString;
+        private MySqlConnection conn;
+
+        public void Connect()
+        {
+            connString = "server=localhost;userid=root;password=admin;";
+            conn = new MySqlConnection(connString);
+
+            conn.Open();
+        }
+
+        public void CreateDatabase()
+        {
+            Connect();
+
+            try
+            {
+                MySqlCommand cmd = new MySqlCommand("create database if not exists Risk", conn);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Database Risk created successfully");
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine("An error occured: {0}", x.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+        }
+
+
+    }
+}
--- JurassicRisk/Views/MainWindow.xaml.cs
+++ JurassicRisk/Views/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using DBStorage;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +25,9 @@ namespace JurassicRisk
         
         public MainWindow()
         {
+            Connection connection = new Connection();
+            connection.Connect();
+            connection.CreateDatabase();
             InitializeComponent();
             frame.NavigationService.Navigate(new HomePage());
         }
