c539c9993bbbe9806ec9bacbc6e9a945d42618f0
vercherebrian <104905297+vercherebrian@users.noreply.github.com>
2022-10-05 16:25:01 +0200

    Ajout des coordonnées pour les territoires

--- Metier/Map/TerritoireBase.cs
+++ Metier/Map/TerritoireBase.cs
@@ -9,10 +9,31 @@ namespace Metier.Map
     public class TerritoireBase
     {
         protected BitmapImage sprite;
+        protected double x;
+        protected double y;
 
         public TerritoireBase(BitmapImage s)
         {
             sprite = s;
+            x = 0;
+            y = 0;
+        }
+
+        public BitmapImage Sprite
+        {
+            get { return sprite; }
+        }
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
         }
     }
 }
