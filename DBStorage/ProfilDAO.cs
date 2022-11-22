1ba586b1693d2d26c636dc15b2d3cb4f6c46185c
Roro1421 <76801282+Roro1421@users.noreply.github.com>
2022-11-22 14:35:58 +0100

    Create ProfilDAO.cs

---
+++ DBStorage/ProfilDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBStorage.ClassMetier;
+
+namespace DBStorage
+{
+    public interface ProfilDAO
+    {
+        public void Insert(Profil profil);
+        public void Update(Profil profil);
+        public void Delete(Profil profil);
+        public string FindByIdProfil(string pseudo);
+        public string FindAll();
+        public bool VerifUserCreation(Profil profil);
+    }
+}
