2233c9b141d053a56687db0f48875a5295a9edeb
vercherebrian <104905297+vercherebrian@users.noreply.github.com>
2022-11-22 10:33:12 +0100

    Création de la Fabrique d'unité

---
+++ JurassicRisk/Units/FabriqueUnite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JurassicRisk.Units
+{
+    public class FabriqueUnite
+    {
+        private Dictionary<string,IMakeUnit> constructors = new Dictionary<string, IMakeUnit>();
+
+        public Unite Create(string type)
+        {
+            if (constructors.ContainsKey(type))
+                return constructors[type].MakeUnit();
+            return null;
+        }
+
+        public String[] Types
+        {
+            get { return constructors.Keys.ToArray(); }
+        }
+    }
+}
