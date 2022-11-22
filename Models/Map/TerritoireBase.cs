cd9fff05bd846c56296421294e8638749e09aa15
vercherebrian <104905297+vercherebrian@users.noreply.github.com>
2022-11-22 13:56:40 +0100

    Ajout de l'attribut troupe dans TerritoireBase. Création de la méthode MaJTroupe -> Ajoute les troupes lors du positionnement

--- Models/Map/TerritoireBase.cs
+++ Models/Map/TerritoireBase.cs
@@ -1,10 +1,13 @@
-﻿namespace Models.Map
+﻿using Models.Units;
+
+namespace Models.Map
 {
     /// <summary>
     /// Classe générique des territoires
     /// </summary>
     public class TerritoireBase
     {
+        private List<IMakeUnit> troupe;
         private Teams _teams;
         public Teams Team
         {
@@ -14,7 +17,15 @@
         }
         public TerritoireBase()
         {
+            troupe = new List<IMakeUnit>();
+        }
 
+        public void MaJTroupe(List<IMakeUnit> unites)
+        {
+            foreach(var unit in unites)
+            {
+                troupe.Add(unit);
+            }
         }
     }
 }
