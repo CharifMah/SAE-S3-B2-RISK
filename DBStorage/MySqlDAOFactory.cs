2fb89bbaa934f589194b2ff2952c5127173ce059
Roro1421 <76801282+Roro1421@users.noreply.github.com>
2022-11-22 14:35:48 +0100

    DBStorage\MySqlDAOFactory.cs
    add class MySqlDAOFactory

---
+++ DBStorage/MySqlDAOFactory.cs
@@ -0,0 +1,35 @@
+﻿using DBStorage.ClassMetier;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBStorage
+{
+    public class MySqlDAOFactory : DAOFactory
+    {
+        private static MySqlDAOFactory Instance;
+        private MySqlDAOFactory() { }
+
+        public static MySqlDAOFactory getInstance()
+        {
+            if (Instance == null)
+            {
+                Instance = new MySqlDAOFactory();
+            }
+            return Instance;
+        }
+        public ProfilDAO CreerProfil()
+        {
+            try
+            {
+                return new ProfilMySqlDAO();
+            }
+            catch(IOException e) 
+            {
+                return null;
+            }
+        }
+    }
+}
