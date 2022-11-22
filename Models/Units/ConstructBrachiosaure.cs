b8287dfede29b8cea924e1fcebe7ec756bfc3430
vercherebrian <104905297+vercherebrian@users.noreply.github.com>
2022-11-22 11:19:14 +0100

    Ajout de la documentation

--- Models/Units/ConstructBrachiosaure.cs
+++ Models/Units/ConstructBrachiosaure.cs
@@ -9,6 +9,11 @@ namespace Models.Units
 {
     public class ConstructBrachiosaure : IMakeUnit
     {
+        /// <summary>
+        /// Instancie un objet de type Brachiosaure
+        /// </summary>
+        /// <returns>Renvoie un Brachiosaure</returns>
+        /// <author>VERCHERE Brian</author>
         public Unite MakeUnit()
         {
             return new Brachiosaure();
--- Models/Units/FabriqueUnite.cs
+++ Models/Units/FabriqueUnite.cs
@@ -10,11 +10,21 @@ namespace Models.Units
     {
         private Dictionary<string, IMakeUnit> constructors = new Dictionary<string, IMakeUnit>();
 
+        /// <summary>
+        /// Initialise les constructeurs d'unité
+        /// </summary>
+        /// <author>VERCHERE Brian</author>
         public FabriqueUnite()
         {
             constructors["brachiosaure"] = new ConstructBrachiosaure();
         }
 
+        /// <summary>
+        /// Créer une unité en fonction du type en paramètre
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns>Renvoie une unité</returns>
+        /// <author>VERCHERE Brian</author>
         public Unite Create(string type)
         {
             if (constructors.ContainsKey(type))
@@ -22,6 +32,10 @@ namespace Models.Units
             return null;
         }
 
+        /// <summary>
+        /// Renvoie tout les types étant considérés comme des unités
+        /// </summary>
+        /// <author>VERCHERE Brian</author>
         public string[] Types
         {
             get { return constructors.Keys.ToArray(); }
