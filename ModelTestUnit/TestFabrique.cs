541e896be7f5ab9c47a65cc63f721344e9810bb5
vercherebrian <104905297+vercherebrian@users.noreply.github.com>
2022-11-22 11:11:00 +0100

    TestUnitaire Fabrique: CréationBrachiosaure

---
+++ ModelTestUnit/TestFabrique.cs
@@ -0,0 +1,20 @@
+﻿using Models.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelTestUnit
+{
+    public class TestFabrique
+    {
+        [Fact]
+        public void CreationBrachiosaure()
+        {
+            FabriqueUnite fabrique = new FabriqueUnite();
+            Unite b1 = fabrique.Create("brachiosaure");
+            Assert.IsType<Brachiosaure>(b1);
+        }
+    }
+}
--- Models/Units/ConstructBrachiosaure.cs
+++ Models/Units/ConstructBrachiosaure.cs
@@ -1,4 +1,4 @@
-﻿using JurassicRisk.Units;
+﻿using Models.Units;
 using System;
 using System.Collections.Generic;
 using System.Linq;
