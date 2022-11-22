56f0919bb9b3b7ee324f323fc5703473c7440e8b
Roro1421 <76801282+Roro1421@users.noreply.github.com>
2022-11-22 16:22:31 +0100

    Update Profil.cs
    add property Id and Mdp

--- DBStorage/ClassMetier/Profil.cs
+++ DBStorage/ClassMetier/Profil.cs
@@ -2,7 +2,11 @@ namespace DBStorage.ClassMetier
 {
     public class Profil
     {
+        public int Id { get; set; }
+
         public string Pseudo { get ; set; }
+
+        public string Mdp { get; set; }
 
         public Profil(string login) 
         {
