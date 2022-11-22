f25bcf81d6fd3a94f0ab0713bf6f6ca656273469
vercherebrian <104905297+vercherebrian@users.noreply.github.com>
2022-11-22 13:53:24 +0100

    Ajout parametre territoire pour la méthode PositionnementTroupe

--- Models/Units/IGestionTroupe.cs
+++ Models/Units/IGestionTroupe.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models.Map;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace Models.Units
 {
     public interface IGestionTroupe
     {
-        public void PositionnerTroupe(List<IMakeUnit> unites);
+        public void PositionnerTroupe(List<IMakeUnit> unites, TerritoireBase territoire);
     }
 }
