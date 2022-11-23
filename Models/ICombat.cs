312a4c01b5206fbdb3429df8684d49a2cfd1f4a0
vercherebrian <104905297+vercherebrian@users.noreply.github.com>
2022-11-23 11:36:46 +0100

    Implémentation interface ICombat

---
+++ Models/ICombat.cs
@@ -0,0 +1,17 @@
+﻿using Models.Map;
+using Models.Units;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public interface ICombat
+    {
+        public void Attaquer(List<Unite> attaquant, TerritoireBase cible);
+
+        public void Defendre(List<Unite> defenseurs, TerritoireBase territoireAttaquant);
+    }
+}
