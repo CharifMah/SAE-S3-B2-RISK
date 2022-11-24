14778d26295a9758744316850c20eee4a8c4b9ea
vercherebrian <104905297+vercherebrian@users.noreply.github.com>
2022-11-24 10:58:37 +0100

    Correction interface ICombat (ajout de public pour les méthodes)

--- Models/ICombat.cs
+++ Models/ICombat.cs
@@ -5,12 +5,12 @@ namespace Models
 {
     public interface ICombat
     {
-        int Attaquer(List<Unite> attaquant, TerritoireBase cible, Joueur assaillant);
-        (int, int) CompareLances(List<int> lanceAttaque, List<int> lanceDefense);
-        int Defendre(List<Unite> defenseur, TerritoireBase territoireAttaquant, Joueur j);
-        void DerouleCombat(List<Unite> attaquant, List<Unite> defenseur, TerritoireBase territoireAttaquant, TerritoireBase cible, Joueur assaillant, Joueur victime);
-        List<int> LancerDes(int nombreDes);
-        void RemoveUnits(int attaqueReussie, int nbAttaque, TerritoireBase territoireAttaquant, TerritoireBase cible);
-        (List<int>, List<int>) TriLances(List<int> attaqueScore, List<int> defenseScore);
+        public int Attaquer(List<Unite> attaquant, TerritoireBase cible, Joueur assaillant);
+        public (int, int) CompareLances(List<int> lanceAttaque, List<int> lanceDefense);
+        public int Defendre(List<Unite> defenseur, TerritoireBase territoireAttaquant, Joueur j);
+        public void DerouleCombat(List<Unite> attaquant, List<Unite> defenseur, TerritoireBase territoireAttaquant, TerritoireBase cible, Joueur assaillant, Joueur victime);
+        public List<int> LancerDes(int nombreDes);
+        public void RemoveUnits(int attaqueReussie, int nbAttaque, TerritoireBase territoireAttaquant, TerritoireBase cible);
+        public (List<int>, List<int>) TriLances(List<int> attaqueScore, List<int> defenseScore);
     }
 }
