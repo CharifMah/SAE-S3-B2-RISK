86498bccf47452236c637efbcb84445fbcfefd68
Roro1421 <76801282+Roro1421@users.noreply.github.com>
2022-11-24 11:35:14 +0100

    Update UsersController.cs
    fix

--- RISKAPI/Controllers/UsersController.cs
+++ RISKAPI/Controllers/UsersController.cs
@@ -75,8 +75,7 @@ namespace RISKAPI.Controllers
                 string[] properties = profilDAO.FindByIdProfil(Id).Split(',');
                 profilDemande = new Profil(properties[1], properties[2]);
                 PasswordHasher<Profil> passwordHasher = new PasswordHasher<Profil>();
-                profil.Password = passwordHasher.HashPassword(profil,profil.Password);
-                if (passwordHasher.VerifyHashedPassword(profilDemande, profilDemande.Password, profil.Password ) == 0)
+                if (passwordHasher.VerifyHashedPassword(profilDemande, profilDemande.Password, profil.Password ) != 0)
                 {
                     actionResult = new JsonResult(profilDemande);
                 }
