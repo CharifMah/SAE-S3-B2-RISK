a40fd8e192f00a056c4eadbc74bd73e1bec7efbc
CharifMah <charif25430@hotmail.fr>
2022-11-25 19:37:00 +0100

    Update MenuPage.xaml.cs

--- JurassicRisk/Views/MenuPage.xaml.cs
+++ JurassicRisk/Views/MenuPage.xaml.cs
@@ -19,7 +19,8 @@ namespace JurassicRisk.Views
         private void PlayButton_Click(object sender, RoutedEventArgs e)
         {
             SoundStore.Get("JungleMusic.mp3").Stop();
-            SoundStore.Get("DarkJungleMusic.mp3").Volume = Settings.Get().Volume / 100;
+            Settings.Get().Backgroundmusic = SoundStore.Get("DarkJungleMusic.mp3");
+            Settings.Get().Backgroundmusic.Volume = Settings.Get().Volume / 100;
             SoundStore.Get("DarkJungleMusic.mp3").Play(true);
             (Window.GetWindow(App.Current.MainWindow) as MainWindow).frame.NavigationService.Navigate(new JeuPage());
         }
