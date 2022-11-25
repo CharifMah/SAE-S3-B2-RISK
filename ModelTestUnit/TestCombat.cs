f048f1f72137fce2b6a4c1e50b5b240ec5b79ddb
CharifMah <charif25430@hotmail.fr>
2022-11-25 15:07:45 +0100

    Combat TestUnit

--- ModelTestUnit/TestCombat.cs
+++ ModelTestUnit/TestCombat.cs
@@ -1,5 +1,6 @@
 ﻿using Models;
 using Models.Combat;
+using Models.Fabriques.FabriqueUnite;
 using Models.Map;
 using Models.Player;
 using Models.Units;
@@ -11,17 +12,17 @@ namespace ModelTestUnit
         [Fact]
         public void Combat()
         {
-            Joueur j1 = new Joueur();
+            Joueur j1 = new Joueur(new Profil("sqd","sqd"),new List<UniteBase>() { new UniteBase(),new UniteBase()},Teams.ROUGE);;
             j1.Equipe = Teams.ROUGE;
             TerritoireBase t1 = new TerritoireBase(1);
             t1.Team = Teams.ROUGE;
 
-            Joueur j2 = new Joueur();
+            Joueur j2 = new Joueur(new Profil("sqdd", "sqdd"), new List<UniteBase>() { new UniteBase(), new UniteBase() }, Teams.ROUGE);
             j2.Equipe = Teams.BLEU;
             TerritoireBase t2 = new TerritoireBase(2);
             t2.Team = Teams.BLEU;
 
-            FabriqueUnite f = new FabriqueUnite();
+            FabriqueUniteBase f = new FabriqueUniteBase();
             for (int i = 0; i < 5; i++)
             {
                 var unit = f.Create("brachiosaure");
