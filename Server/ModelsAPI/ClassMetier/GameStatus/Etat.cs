d7060c440aedad7395227b769dfc61fcf47c10df
Roro1421 <76801282+Roro1421@users.noreply.github.com>
2023-01-16 14:14:02 +0100

    Add deploiment and transition modification

--- Server/ModelsAPI/ClassMetier/GameStatus/Etat.cs
+++ Server/ModelsAPI/ClassMetier/GameStatus/Etat.cs
@@ -11,7 +11,7 @@ namespace ModelsAPI.ClassMetier.GameStatus
 {
     public interface Etat
     {
-        public Etat TransitionTo(List<Joueur> joueurs);
+        public Etat TransitionTo(List<Joueur> joueurs, Carte carte);
 
         public void Action(Carte carte, Joueur joueur, List<int> unitList);
     }
