d2cbfdf49d1e9b8eab47cc6ea6a77637910400a9
Roro1421 <76801282+Roro1421@users.noreply.github.com>
2023-01-16 13:49:56 +0100

    Fix attaque.toString

--- Server/ModelsAPI/ClassMetier/GameStatus/Attaque.cs
+++ Server/ModelsAPI/ClassMetier/GameStatus/Attaque.cs
@@ -22,7 +22,7 @@ namespace ModelsAPI.ClassMetier.GameStatus
         }
         public override string? ToString()
         {
-            return "Deploiment";
+            return "Attaque";
         }
 
     }
